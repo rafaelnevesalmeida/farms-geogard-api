9e6931f302ec3d86414e18ee92b402aa04e95c06
rafael <rafaelnevesalmeida@gmail.com>
2018-04-22 15:00:11 -0300

    replace origin address permit to get from env

--- server.js
+++ server.js
@@ -5,23 +5,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const schema = require('./data/schema')
-// const jwt = require('express-jwt')
 require('dotenv').config()
 
 const PORT = 4000
 
 const app = express()
-app.use('*', cors({ origin: ['http://localhost:3000', '189.13.255.152:3000'] }))
-// app.use('/api', bodyParser.json(), graphqlExpress(req => ({
-// schema // ,
-// context: {
-// authUser: req.user
-// }
-// })))
+app.use('*', cors({ origin: process.env.ORIGIN }))
 
 app.use('/api', bodyParser.json(), graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: 'api' }))
 
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`GraphiQL is running on http://localhost:${PORT}/graphiql`)
+  console.log(`GraphiQL is running on .../graphiql`)
+  console.log(process.env.ORIGIN)
 })
